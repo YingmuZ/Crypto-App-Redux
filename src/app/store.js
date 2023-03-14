6edbd49225208b673a55b07a2bfef8fa27633fec
yingmu <wawamu2201@gmail.com>
2023-03-14 23:44:47 +0100

    data bug

--- src/app/store.js
+++ src/app/store.js
@@ -1,12 +1,39 @@
 import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
 
 import { cryptoApi } from "../services/cryptoApi";
+import { cryptoNewsApi } from "../services/cryptoNewsApi";
 
-export default configureStore({
+const store = configureStore({
     reducer: {
-        [cryptoApi.reducerPath]: cryptoApi.reducer,
+    [cryptoApi.reducerPath]: cryptoApi.reducer,
     },
+
     middleware: getDefaultMiddleware => 
-        getDefaultMiddleware().concat(cryptoApi.middleware)
+        getDefaultMiddleware().concat(cryptoApi.middleware),
     
 });
+
+const storeNews = configureStore({
+    reducer: {
+  
+    [cryptoNewsApi.reducerPath]: cryptoNewsApi.reducer,
+    },
+
+    middleware: getDefaultMiddleware => 
+        getDefaultMiddleware().concat(cryptoNewsApi.middleware),
+    
+});
+
+export { store, storeNews };
+
+// export default configureStore({
+//     reducer: {
+//         [cryptoApi.reducerPath]: cryptoApi.reducer,
+       
+//     },
+//     middleware: getDefaultMiddleware => 
+//         getDefaultMiddleware().concat(cryptoApi.middleware)
+    
+// });
+
+
