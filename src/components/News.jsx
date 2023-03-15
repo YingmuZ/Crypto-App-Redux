1d5bff597088256382d5e70f7fbac083a1c8b97c
yingmu <wawamu2201@gmail.com>
2023-03-15 22:55:20 +0100

    newspage filter function

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -1,24 +1,74 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Select, Typography, Row, Col, Avatar, Card } from 'antd';
 import moment from 'moment';
 
 import { useGetCryptoNewsQuery } from '../services/cryptoNewsApi';
+import { useGetCryptosQuery } from '../services/cryptoApi';
 
 const { Text, Title } = Typography;
 const { Option } = Select;
 
+const demoImage = 'https://www.bing.com/th?id=OVFT.mpzuVZnv8dwIMRfQGPbOPC&pid=News';
+
 
 const News = ({ simplified }) => {
+  const [newsCategory, setNewsCategory] = useState("Cryptocurrency") //这里是initialState
   //fetch the data 
-  
-  const { data: cryptoNews } = useGetCryptoNewsQuery({ newsCategory: "Cryptocurrency", count: simplified ? 5 : 50});
-  
-  console.log(cryptoNews);
+  const { data: cryptoNews } = useGetCryptoNewsQuery({ newsCategory, count: simplified ? 5 : 12}); 
+  //console.log(cryptoNews);
+  const { data } = useGetCryptosQuery(50);
+
+  if(!cryptoNews?.value) return "loading...";
  
   return (
-    <div>
-      
-    </div>
+    <Row gutter={[ 24, 24 ]}>
+      {/* if no simplified then render... */}
+      {!simplified && (
+        <Col span={24}>
+          <Select
+            showSearch
+            className='select-news'
+            placeholder="search a crypto"
+            optionFilterProp='children'
+            // onChange={(value) => console.log(value)}
+            onChange={(value) => setNewsCategory(value)}
+            // filtering out the options so that only show the one for the selected cryptocurrency
+            // ofc we need to create a useState for the crypto category
+ 
+          >
+            <Option value="Cryptocurrency">cryptocurrency</Option>
+            {data?.data?.coins.map((coin) => <Option value={coin.name}>{coin.name}</Option>)}
+          </Select>
+        </Col>
+      )}
+
+      {cryptoNews.value.map((news, i) => (
+        <Col xs={24} sm={12} lg={8} key={i}>
+            <Card hoverable className='news-card'>
+              <a href={news.url} target="_blank" rel="noreferrer">
+                <div className='news-image-container'>
+                    <Title className='news-title' level={4}>{news.name}</Title>
+                    <img style={{ maxWidth: "200px", maxHeight: "100px" }} src={news?.image?.thumbnail?.contentUrl || demoImage } alt="news" />
+                </div>
+                <p>
+                  {news.description > 100
+                    ? `${news.description.substring(0, 100)}...`
+                    : news.description
+                  }
+                </p>
+                <div className='provider-container'>
+                  <div>
+                    <Avatar src={news.provider[0]?.image?.thumbnail?.contentUrl || demoImage} alt="" />
+                    <Text className='provider-name'>{news.provider[0]?.name}</Text>
+                  </div>
+                  <Text>{moment(news.datePublished).startOf("ss").fromNow()}</Text>
+                </div>
+              </a>
+            </Card>
+        </Col>
+      ))}
+
+    </Row>
   )
 }
 
